33a7aae1aa2fd60119cbefc86233a56bf3383cb1
Kai Schmidt <kaikaliischmidt@gmail.com>
2018-08-13 16:36:39 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,158 @@
+extern crate graphics;
+extern crate image;
+
+use std::ops;
+
+use graphics::{draw_state::DrawState, types::Color, Graphics, ImageSize};
+use image::{Rgba, RgbaImage};
+
+fn color_f32_rgba(color: &[f32; 4]) -> Rgba<u8> {
+    Rgba {
+        data: [
+            (color[0] * 255.0) as u8,
+            (color[1] * 255.0) as u8,
+            (color[2] * 255.0) as u8,
+            (color[3] * 255.0) as u8,
+        ],
+    }
+}
+
+fn color_rgba_f32(color: &Rgba<u8>) -> [f32; 4] {
+    [
+        (color.data[0] as f32) / 255.0,
+        (color.data[1] as f32) / 255.0,
+        (color.data[2] as f32) / 255.0,
+        (color.data[3] as f32) / 255.0,
+    ]
+}
+
+fn sign(p1: [f32; 2], p2: [f32; 2], p3: [f32; 2]) -> f32 {
+    (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
+}
+
+fn triangle_contains(a: [f32; 2], b: [f32; 2], c: [f32; 2], point: [f32; 2]) -> bool {
+    let b1 = sign(point, a, b) < 0.0;
+    let b2 = sign(point, b, c) < 0.0;
+    let b3 = sign(point, c, a) < 0.0;
+    b1 == b2 && b2 == b3
+}
+
+#[derive(Debug, Clone)]
+pub struct RenderBuffer {
+    inner: RgbaImage,
+}
+
+impl RenderBuffer {
+    pub fn new(width: u32, height: u32) -> RenderBuffer {
+        RenderBuffer {
+            inner: RgbaImage::new(width, height),
+        }
+    }
+    pub fn clear(&mut self, color: [f32; 4]) {
+        self.clear_color(color);
+    }
+    pub fn pixel(&self, x: u32, y: u32) -> [f32; 4] {
+        color_rgba_f32(self.inner.get_pixel(x, y))
+    }
+    pub fn print(&self) {
+        for x in 0..self.width() {
+            if x % 2 == 1 {
+                continue;
+            }
+            for y in 0..self.height() {
+                print!("{}", if self.pixel(x, y)[0] == 0.0 { "." } else { "X" });
+            }
+            println!()
+        }
+    }
+}
+
+impl From<RgbaImage> for RenderBuffer {
+    fn from(image: RgbaImage) -> Self {
+        RenderBuffer { inner: image }
+    }
+}
+
+impl ops::Deref for RenderBuffer {
+    type Target = RgbaImage;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl ImageSize for RenderBuffer {
+    fn get_size(&self) -> (u32, u32) {
+        self.inner.dimensions()
+    }
+}
+
+impl Graphics for RenderBuffer {
+    type Texture = RenderBuffer;
+    fn clear_color(&mut self, color: Color) {
+        for (_, _, pixel) in self.inner.enumerate_pixels_mut() {
+            *pixel = color_f32_rgba(&color);
+        }
+    }
+    fn clear_stencil(&mut self, _value: u8) {}
+    fn tri_list<F>(&mut self, _draw_state: &DrawState, color: &[f32; 4], mut f: F)
+    where
+        F: FnMut(&mut FnMut(&[[f32; 2]])),
+    {
+        // Convert color
+        let color = color_f32_rgba(color);
+        f(&mut |vertices| {
+            for tri in vertices.chunks(3) {
+                // Get tri bounds for efficiency
+                let mut tl = [0f32, 0f32];
+                let mut br = [0f32, 0f32];
+                for v in tri {
+                    tl[0] = tl[0].min(v[0]);
+                    tl[1] = tl[1].min(v[1]);
+                    br[0] = br[0].max(v[0]);
+                    br[1] = br[1].max(v[1]);
+                }
+                let tl = [tl[0].floor() as u32, tl[1].floor() as u32];
+                let br = [br[0].ceil() as u32, br[1].ceil() as u32];
+                for x in tl[0]..br[0] {
+                    for y in tl[1]..br[1] {
+                        if triangle_contains(tri[0], tri[1], tri[2], [x as f32, y as f32]) {
+                            self.inner.put_pixel(x, y, color);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    fn tri_list_uv<F>(
+        &mut self,
+        _draw_state: &DrawState,
+        _color: &[f32; 4],
+        _texture: &Self::Texture,
+        _f: F,
+    ) where
+        F: FnMut(&mut FnMut(&[[f32; 2]], &[[f32; 2]])),
+    {
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use graphics::ellipse;
+    use image::{png::PNGEncoder, ColorType};
+    use std::fs::File;
+    #[test]
+    fn circle_test() {
+        let mut rb = RenderBuffer::new(100, 100);
+        rb.clear([0.0, 0.0, 0.0, 1.0]);
+        ellipse(
+            [1.0, 0.0, 0.0, 1.0],
+            [0.0, 0.0, 50.0, 50.0],
+            [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]],
+            &mut rb,
+        );
+        PNGEncoder::new(File::create("circle.png").unwrap())
+            .encode(&*rb, rb.width(), rb.height(), ColorType::RGBA(8))
+            .expect("Unable to encode image");
+    }
+}
