6b38e28c772f0a245f108fbf0b8f82bbd99ca2e3
Kai Schmidt <kaikaliischmidt@gmail.com>
2018-08-14 00:22:04 -0700

    added comments to circles example

--- examples/circles.rs
+++ examples/circles.rs
@@ -6,25 +6,32 @@ use graphics::ellipse;
 use graphics_buffer::*;
 
 fn main() {
+    // Create a new RenderBuffer
     let mut buffer = RenderBuffer::new(100, 100);
     buffer.clear([0.0, 0.0, 0.0, 1.0]);
+
+    // Big red circle
     ellipse(
         [1.0, 0.0, 0.0, 1.0],
         [0.0, 0.0, 100.0, 100.0],
         identity(),
         &mut buffer,
     );
+    // Small blue circle
     ellipse(
         [0.0, 0.0, 1.0, 0.5],
         [0.0, 0.0, 50.0, 50.0],
         identity(),
         &mut buffer,
     );
+    // Small green circle
     ellipse(
         [0.0, 1.0, 0.0, 0.5],
         [50.0, 50.0, 50.0, 50.0],
         identity(),
         &mut buffer,
     );
+
+    // Save the buffer
     buffer.save("circles.png").unwrap();
 }
