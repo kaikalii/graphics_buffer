3253be0cca339907ce1015dcd551f36217dc874a
Kai Schmidt <kaikaliischmidt@gmail.com>
2018-08-13 22:00:22 -0700

    texture mapping works

--- src/lib.rs
+++ src/lib.rs
@@ -37,25 +37,17 @@ fn triangle_contains(tri: &[[f32; 2]], point: [f32; 2]) -> bool {
     b1 == b2 && b2 == b3
 }
 
-fn dist(a: [f32; 2], b: [f32; 2]) -> f32 {
-    ((a[0] - b[0]).powf(2.0) + (a[1] - b[1]).powf(2.0)).powf(0.5)
-}
-
 fn map_to_triangle(point: [f32; 2], from_tri: &[[f32; 2]], to_tri: &[[f32; 2]]) -> [f32; 2] {
-    let dists = [
-        dist(from_tri[0], point),
-        dist(from_tri[1], point),
-        dist(from_tri[2], point),
-    ];
-    let dist_sum = dists[0] + dists[1] + dists[2];
-    let sp = [
-        [to_tri[0][0] * dists[0], to_tri[0][1] * dists[0]],
-        [to_tri[1][0] * dists[0], to_tri[1][1] * dists[0]],
-        [to_tri[2][0] * dists[0], to_tri[2][1] * dists[0]],
-    ];
+    let t = from_tri;
+    let p = point;
+    let bary_a = ((t[1][1] - t[2][1]) * (p[0] - t[2][0]) + (t[2][0] - t[1][0]) * (p[1] - t[2][1]))
+        / ((t[1][1] - t[2][1]) * (t[0][0] - t[2][0]) + (t[2][0] - t[1][0]) * (t[0][1] - t[2][1]));
+    let bary_b = ((t[2][1] - t[0][1]) * (p[0] - t[2][0]) + (t[0][0] - t[2][0]) * (p[1] - t[2][1]))
+        / ((t[1][1] - t[2][1]) * (t[0][0] - t[2][0]) + (t[2][0] - t[1][0]) * (t[0][1] - t[2][1]));
+    let bary_c = 1.0 - bary_a - bary_b;
     [
-        (sp[0][0] + sp[1][0] + sp[2][0]) / dist_sum,
-        (sp[0][1] + sp[1][1] + sp[2][1]) / dist_sum,
+        bary_a * to_tri[0][0] + bary_b * to_tri[1][0] + bary_c * to_tri[2][0],
+        bary_a * to_tri[0][1] + bary_b * to_tri[1][1] + bary_c * to_tri[2][1],
     ]
 }
 
@@ -87,17 +79,6 @@ impl RenderBuffer {
     }
     pub fn pixel(&self, x: u32, y: u32) -> [f32; 4] {
         color_rgba_f32(self.inner.get_pixel(x, y))
-    }
-    pub fn print(&self) {
-        for x in 0..self.width() {
-            if x % 2 == 1 {
-                continue;
-            }
-            for y in 0..self.height() {
-                print!("{}", if self.pixel(x, y)[0] == 0.0 { "." } else { "X" });
-            }
-            println!()
-        }
     }
 }
 
@@ -191,15 +172,15 @@ impl Graphics for RenderBuffer {
                 let tl = [tl[0].floor() as u32, tl[1].floor() as u32];
                 let br = [br[0].ceil() as u32, br[1].ceil() as u32];
                 // Render
+                let scaled_tex_tri = tri_image_scale(tex_tri, texture.get_size());
                 for x in tl[0]..br[0] {
                     for y in tl[1]..br[1] {
                         if triangle_contains(tri, [x as f32, y as f32]) {
-                            let scaled_tex_tri = tri_image_scale(tex_tri, texture.get_size());
                             let mapped_point =
                                 map_to_triangle([x as f32, y as f32], tri, &scaled_tex_tri);
                             let texel = texture.get_pixel(
-                                (mapped_point[0] as u32).min(texture.get_width() - 1),
-                                (mapped_point[1] as u32).min(texture.get_height() - 1),
+                                mapped_point[0].round() as u32,
+                                mapped_point[1].round() as u32,
                             );
                             self.inner.put_pixel(x, y, *texel);
                         }
@@ -207,32 +188,5 @@ impl Graphics for RenderBuffer {
                 }
             }
         });
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use graphics::{ellipse, image as draw_image};
-    use image;
-    #[test]
-    fn circle_test() {
-        let mut rb = RenderBuffer::new(100, 100);
-        rb.clear([0.0, 0.0, 0.0, 1.0]);
-        ellipse(
-            [1.0, 0.0, 0.0, 1.0],
-            [0.0, 0.0, 50.0, 50.0],
-            [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]],
-            &mut rb,
-        );
-        rb.save("circle.png").unwrap();
-    }
-    #[test]
-    fn image_test() {
-        let texture = RenderBuffer::from(image::open("squares.png").unwrap());
-        let mut rb = RenderBuffer::new(100, 100);
-        rb.clear([0.0, 0.0, 0.0, 1.0]);
-        draw_image(&texture, [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]], &mut rb);
-        rb.save("red_matt.png").unwrap();
     }
 }
